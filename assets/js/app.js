9cdf3ce24947c7ccac064be45033eec7941529a8
nonteku <akamckokou@gmail.com>
2019-05-30 06:46:03 +0900

    2ページ目 sassでのcss、js、追加アニメーション、レスポンシブ対応　完了！！！

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,10 @@
 $( document ).ready(function() {
-    var image = $('.img-profile');
-    var aboutPosition = $('.about-myself').offset().top/1.5;
+    let image = $('.img-profile');
+    let aboutPosition = $('.about-myself').offset().top/1.5;
   
     $(window).on('scroll', function () {
   
-      var scrollTop = $(this).scrollTop();
+      let scrollTop = $(this).scrollTop();
       image.css({ 'top': (aboutPosition - scrollTop) });
   
       if(scrollTop > aboutPosition){
@@ -12,19 +12,57 @@ $( document ).ready(function() {
       }
   
   
-      var fadeInTime = 0;
+      let fadeInTime = 0;
   
       $('.service').each( function(i){
   
-          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-          var bottom_of_window = $(window).scrollTop() + $(window).height();
+          let bottom_of_object = $(this).offset().top + $(this).outerHeight();
+          let bottom_of_window = $(window).scrollTop() + $(window).height();
   
           if( bottom_of_window > bottom_of_object/1.25 ){
   
-            $(this).delay(fadeInTime).animate({'opacity':'1'},1000);
-             fadeInTime = fadeInTime + 400;
+            $(this).delay(fadeInTime).animate({'opacity':'1'},2000);
+             fadeInTime = fadeInTime + 1000;
           }
       });
     });
     
+    
+    //画像がクリックされたら、div＃overを追加
+    　
+    　$('.img-profile').click(function(){
+   
+       $('<div id="over">').appendTo($('.about-myself')).hide().fadeIn();
+   
+       $('#over').append($('<div class="img-box">'))
+       $('.img-box').append($('<span class="close">&times;</span>'))
+       $('.img-box').append($('<img class="lightbox"  width="500px">'))
+   
+       //クリックされた画像のsrcを取得する
+      //  let imgSrc = $(this).attr('src');
+       let imgSrc = $(this).attr('src');
+       //作成した<img class="lightbox">のsrcにクリックされた画像のsrcを設定
+       $('.lightbox').attr('src', imgSrc);
+   
+       // .closeがクリックされたら、img-boxを消す
+       $('.close').click(function() {
+   
+         $('.img-box').fadeOut('slow', function() {
+         
+           $('#over').fadeOut('fast', function() {
+         
+            $('#over').remove();
+   
+           })
+         })
+       })
+   
+   
+     })
+
+
+
+
+
+
   });
